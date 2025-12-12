ab524c36c40f279213d481412c2b900aa46e5ee2
David Diehr <dmdiehr@outlook.com>
2025-12-12 13:55:41 -0800

    Finish renaming

--- StardewSeedSearch.Core/GemBirdPredictor.cs
+++ StardewSeedSearch.Core/GemBirdPredictor.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using StardewSeedSearch.Core.Helpers;
 
 namespace StardewSeedSearch.Core;
 
--- StardewSeedSearch.Core/GreenRainPredictor.cs
+++ StardewSeedSearch.Core/GreenRainPredictor.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using StardewSeedSearch.Core.Helpers;
 
 namespace StardewSeedSearch.Core;
 
